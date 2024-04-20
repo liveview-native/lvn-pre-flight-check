58de868745c5e99fd13907e81088c6fb84f85b6b
Brian Cardarella <bcardarella@gmail.com>
2024-04-20 10:24:34 -0400

    Initial Commit

---
+++ LVN Pre-Flight Check/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LVN Pre-Flight Check
+//
+//  Created by Brian Cardarella on 4/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LVN Pre-Flight Check/LVN_Pre_Flight_CheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  LVN_Pre_Flight_CheckApp.swift
+//  LVN Pre-Flight Check
+//
+//  Created by Brian Cardarella on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct LVN_Pre_Flight_CheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
